833b83a88c4eea59337957f713aa268c898077bf
100xlong <100xlong@100xlongs-MBP.attlocal.net>
2022-06-04 18:29:59 -0400

    death

--- engine.py
+++ engine.py
@@ -4,22 +4,21 @@ from typing import TYPE_CHECKING
 from tcod.context import Context
 from tcod.console import Console
 
-from actions import EscapeAction, MovementAction
-from entity import Entity
-from input_handlers import EventHandler
+from input_handlers import MainGameEventHandler
 from game_map import GameMap
 from tcod.map import compute_fov
 
 if TYPE_CHECKING:
-    from entity import Entity
     from game_map import GameMap
+    from entity import Actor
+    from input_handlers import EventHandler
 
 
 class Engine:
     game_map: GameMap
 
-    def __init__(self, player: Entity):
-        self.event_handler: EventHandler = EventHandler(self)
+    def __init__(self, player: Actor):
+        self.event_handler: EventHandler = MainGameEventHandler(self)
         self.player = player
 
     def handle_enemy_turns(self) -> None:
@@ -39,5 +38,12 @@ class Engine:
 
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
+
+        console.print(
+            x=1,
+            y=47,
+            string=f"HP: {self.player.fighter.hp}/{self.player.fighter.max_hp}",
+        )
+
         context.present(console)
         console.clear()

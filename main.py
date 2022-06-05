8dde7b3e24199d725191ac6c44591441c30794f1
100xlong <100xlongsol@gmail.com>
2022-06-05 18:17:37 -0400

    assets folder

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main() -> None:
     screen_height = 50
 
     tileset = tcod.tileset.load_tilesheet(
-        "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+        "assets/dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
     handler: input_handlers.BaseEventHandler = setup_game.MainMenu()
